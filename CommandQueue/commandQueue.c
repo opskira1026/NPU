d2927fcca6bf1bf8004dbd2a032923ee5e62d9ce
JUNOO1026 <opskira1026@gmail.com>
2026-07-27 23:45:50 +0900

    기존의 싱글 링크드리스트에 더미 노드를 추가하여 로직을 좀 더 간단하게 구현

--- CommandQueue/commandQueue.c
+++ CommandQueue/commandQueue.c
@@ -10,7 +10,10 @@ typedef struct COMMAND
 
 } COMMAND;
 
-COMMAND* g_pHeadCommand = NULL;
+// 해당 방식으로 싱글 링크드리스트를 사용하면, 조건이 많아지는 문제가 있음.
+// 따라서 더미 헤드를 사용하여 조건을 줄이는 방향으로 로직을 구성.
+//COMMAND* g_pHeadCommand = NULL;
+COMMAND g_DummyHead = { 0, "__DUMMY HEAD__" };
 
 void enqueue(int i, const char* op)
 {
@@ -26,14 +29,24 @@ void enqueue(int i, const char* op)
 	strcpy_s(newCommand->operation, sizeof(newCommand->operation), op);
 	newCommand->pNext = NULL;
 
-	if (g_pHeadCommand == NULL)
-		g_pHeadCommand = newCommand;
+	//if (g_pHeadCommand == NULL)
+	//	g_pHeadCommand = newCommand;
+	//else
+	//{
+
+	//	// 현재 구현은 Tail을 매번 탐색하므로 enqueue가 O(N) 
+	//	// 실제 Runtime에서는 Tail 포인터를 함께 관리하여 O(1)로 구현한다.
+	//	COMMAND* pTailCommand = g_pHeadCommand;
+	//	while (pTailCommand->pNext != NULL)
+	//		pTailCommand = pTailCommand->pNext;
+	//	pTailCommand->pNext = newCommand;
+	//}
+
+	if (g_DummyHead.pNext == NULL)
+		g_DummyHead.pNext = newCommand;
 	else
 	{
-
-		// 현재 구현은 Tail을 매번 탐색하므로 enqueue가 O(N) 
-		// 실제 Runtime에서는 Tail 포인터를 함께 관리하여 O(1)로 구현한다.
-		COMMAND* pTailCommand = g_pHeadCommand;
+		COMMAND* pTailCommand = g_DummyHead.pNext;
 		while (pTailCommand->pNext != NULL)
 			pTailCommand = pTailCommand->pNext;
 		pTailCommand->pNext = newCommand;
@@ -42,15 +55,15 @@ void enqueue(int i, const char* op)
 
 COMMAND* dequeueCommand(void)
 {
-	COMMAND* pHeadCommand = g_pHeadCommand;
+	COMMAND* pHeadCommand = g_DummyHead.pNext;
 	COMMAND* pRemoveCommand = NULL;
 
 	if (pHeadCommand == NULL)
 		return NULL;
 	else
 	{
-		pRemoveCommand = g_pHeadCommand;
-		g_pHeadCommand = pHeadCommand->pNext;
+		pRemoveCommand = g_DummyHead.pNext;
+		g_DummyHead.pNext = pHeadCommand->pNext;
 	}
 	//지워질 대상이니 다음 노드를 가리킬 필요없음
 	pRemoveCommand->pNext = NULL;
@@ -70,7 +83,7 @@ void destroyCommand(COMMAND* pRemove)
 
 void releaseQueue(void)
 {
-	COMMAND* pTmp = g_pHeadCommand;
+	COMMAND* pTmp = g_DummyHead.pNext;
 	COMMAND* pDelete = NULL;
 
 	while (pTmp != NULL)
@@ -93,6 +106,7 @@ void executeCommand(COMMAND* pRemove)
 	if (pRemove == NULL)
 		return;
 
+	putchar('\n');
 	printf("[NPU]\n");
 	printf("EXECUTE Command\n");
 	printf("ID : %d\n", pRemove->commandID);
@@ -110,7 +124,7 @@ void fcreateCommand(void)
 
 void printCommand(void)
 {
-	COMMAND* printCommand = g_pHeadCommand;
+	COMMAND* printCommand = g_DummyHead.pNext;
 	
 	printf("Current Queue\n");
 	printf("------------------------------------------------------\n");
@@ -128,7 +142,7 @@ void printCommand(void)
 
 	// 반복문이 끝나면 pTmp는 항상 참
 	// g_pHeadCommand가 NULL일 경우에 빈 Queue를 출력
-	if (g_pHeadCommand == NULL)
+	if (g_DummyHead.pNext == NULL)
 		printf("Queue Empty\n");
 }
 
