ca2d653ca4365f7273fa9224d63ffe86e8568f7d
JUNOO1026 <opskira1026@gmail.com>
2026-07-29 23:14:57 +0900

    header+data 구조 구현중

--- MemoryAllocatorFreeList/MemoryAllocatorFreeList/MemoryAllocatorFreeList/MemoryAllocatorFreeList.c
+++ MemoryAllocatorFreeList/MemoryAllocatorFreeList/MemoryAllocatorFreeList/MemoryAllocatorFreeList.c
@@ -2,26 +2,27 @@
 
 #define HEAP_SIZE 1024
 
-typedef enum BLOCKSTATE
+typedef enum BLOCK_STATE
 {
 	FREE,
 	USER
 
-} BLOCKSTATE;
+} BLOCK_STATE;
 
 typedef struct BLOCK
 {
-	int size;
-	BLOCKSTATE state;
+	size_t size;
+	BLOCK_STATE state;
 
 	struct BLOCK* pPrev;
 	struct BLOCK* pNext;
 } BLOCK;
 
 unsigned char g_heap[HEAP_SIZE];
+BLOCK g_TailDummyNode = { 0 };
 BLOCK* g_pFirstBlock = NULL;
 
-const char* getStateString(int state)
+const char* getStateString(BLOCK_STATE state)
 {
 	switch (state)
 	{
@@ -36,20 +37,64 @@ const char* getStateString(int state)
 
 void Init(void)
 {
-	BLOCK* firstBlock = (BLOCK*)g_heap;
+	BLOCK* InitBlock = (BLOCK*)g_heap;
 
-	firstBlock->size = HEAP_SIZE;
-	firstBlock->state = 0;
-	firstBlock->pPrev = NULL;
-	firstBlock->pNext = NULL;
+	// 메모리의 실제 값은 1024가 아닌 BLOCK 값을 빼줘야 함
+	// header 정보 또한 메모리에 저장되므로 그 값을 빼줘야 
+	// 오버플로우가 발생하지 않음
+	InitBlock->size = HEAP_SIZE - sizeof(BLOCK);
+	InitBlock->state = FREE;
+	InitBlock->pPrev = NULL;
+	InitBlock->pNext = NULL;
 
-	g_pFirstBlock = firstBlock;
+	g_pFirstBlock = InitBlock;
+
+	printf("---------------------------------\n");
+	printf("|\t\t%s\t\t|\n", getStateString(InitBlock->state));
+	printf("---------------------------------\n\n");
+
+}
+
+void myMalloc(BLOCK* header)
+{
+	BLOCK* newBlock = (BLOCK*)(char*)header + sizeof(header) + 
+
+}
+
+void printHeap(BLOCK* blk)
+{
+	int i = 0;
+	BLOCK* pTmp = g_pFirstBlock;
+	
+	//printf("%zu\n", sizeof(BLOCK));
+	//printf("Address : %p", (char*)blk + sizeof(BLOCK));
+
+	while (pTmp != NULL)
+	{
+		printf("------------------------------\n");
+		printf("block : %d\n", i++);
+		printf("block size : %zu\n", sizeof(BLOCK));
+		printf("address : %p\n", (char*)blk + sizeof(blk));
+		printf("size : %zd\n", blk->size);
+		printf("state : %s\n", getStateString(blk->state));
+		printf("------------------------------\n\n");
+		break;
+		
+	
+	}
+
+
+	//while (g_pFirstBlock != NULL)
+	//{
+
+	//	g_pFirstBlock = g_pFirstBlock->pNext;
+	//}
 }
 
 int main(void)
 {
 	Init();
-
+	printHeap(g_pFirstBlock);
 	/*printf("%d %s %p %p",
 		g_pFirstBlock->size, getStateString(g_pFirstBlock->state),
 		g_pFirstBlock->pPrev, g_pFirstBlock->pNext);*/
