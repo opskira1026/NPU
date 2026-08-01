850441e34880584738580a31849b8ba4b7bce4c2
JUNOO1026 <opskira1026@gmail.com>
2026-08-01 21:09:08 +0900

    Memory Allocator Free List 기본 구조 구현

--- MemoryAllocatorFreeList/MemoryAllocatorFreeList/MemoryAllocatorFreeList/MemoryAllocatorFreeList.c
+++ MemoryAllocatorFreeList/MemoryAllocatorFreeList/MemoryAllocatorFreeList/MemoryAllocatorFreeList.c
@@ -4,102 +4,195 @@
 
 typedef enum BLOCK_STATE
 {
-	FREE,
-	USER
+    FREE,
+    USED
 
-} BLOCK_STATE;
+}BLOCK_STATE;
 
 typedef struct BLOCK
 {
-	size_t size;
-	BLOCK_STATE state;
+    size_t size;
 
-	struct BLOCK* pPrev;
-	struct BLOCK* pNext;
-} BLOCK;
+    BLOCK_STATE state;
+
+    struct BLOCK* pPrev;
+    struct BLOCK* pNext;
+
+}BLOCK;
 
 unsigned char g_heap[HEAP_SIZE];
-BLOCK g_TailDummyNode = { 0 };
+
 BLOCK* g_pFirstBlock = NULL;
 
 const char* getStateString(BLOCK_STATE state)
 {
-	switch (state)
-	{
-	case FREE:
-		return "FREE";
-	case USER:
-		return "USER";
-	default:
-		return "UNKNOWN";
-	}
+    switch (state)
+    {
+    case FREE:
+        return "FREE";
+
+    case USED:
+        return "USED";
+
+    default:
+        return "UNKNOWN";
+    }
 }
 
-void Init(void)
+void initAllocator(void)
 {
-	BLOCK* InitBlock = (BLOCK*)g_heap;
+    BLOCK* pBlock = (BLOCK*)g_heap;
 
-	// 메모리의 실제 값은 1024가 아닌 BLOCK 값을 빼줘야 함
-	// header 정보 또한 메모리에 저장되므로 그 값을 빼줘야 
-	// 오버플로우가 발생하지 않음
-	InitBlock->size = HEAP_SIZE - sizeof(BLOCK);
-	InitBlock->state = FREE;
-	InitBlock->pPrev = NULL;
-	InitBlock->pNext = NULL;
+    pBlock->size = HEAP_SIZE - sizeof(BLOCK);
+    pBlock->state = FREE;
 
-	g_pFirstBlock = InitBlock;
+    pBlock->pPrev = NULL;
+    pBlock->pNext = NULL;
 
-	printf("---------------------------------\n");
-	printf("|\t\t%s\t\t|\n", getStateString(InitBlock->state));
-	printf("---------------------------------\n\n");
-
+    g_pFirstBlock = pBlock;
 }
 
-void myMalloc(BLOCK* header)
+void* myMalloc(size_t size)
 {
-	BLOCK* newBlock = (BLOCK*)(char*)header + sizeof(header) + 
+    BLOCK* current = g_pFirstBlock;
 
+    while (current != NULL)
+    {
+        // 사용 가능한 Block인가?
+        if (current->state == FREE && current->size >= size)
+        {
+            size_t remain = current->size - size;
+
+            if (remain > sizeof(BLOCK))
+            {
+                BLOCK* newBlock = (BLOCK*)((char*)current + sizeof(BLOCK) + size);
+
+                newBlock->size = remain - sizeof(BLOCK);
+                newBlock->state = FREE;
+
+                newBlock->pPrev = current;
+                newBlock->pNext = current->pNext;
+
+                if (current->pNext != NULL)
+                {
+                    current->pNext->pPrev = newBlock;
+                }
+                current->pNext = newBlock;
+                current->size = size;
+            }
+            current->state = USED;
+
+            return (void*)((char*)current + sizeof(BLOCK));
+        }
+        current = current->pNext;
+    }
+    return NULL;
+}
+void mergeBlock(BLOCK* current)
+{
+    if (current == NULL)
+        return;
+
+    BLOCK* next = current->pNext;
+
+    if (next == NULL)
+        return;
+
+    if (current->state == FREE &&
+        next->state == FREE)
+    {
+        current->size = current->size + sizeof(BLOCK) + next->size;
+        current->pNext = next->pNext;
+
+        if (next->pNext != NULL)
+        {
+            next->pNext->pPrev = current;
+        }
+    }
 }
 
-void printHeap(BLOCK* blk)
+void myFree(void* ptr)
 {
-	int i = 0;
-	BLOCK* pTmp = g_pFirstBlock;
-	
-	//printf("%zu\n", sizeof(BLOCK));
-	//printf("Address : %p", (char*)blk + sizeof(BLOCK));
+    if (ptr == NULL)
+        return;
 
-	while (pTmp != NULL)
-	{
-		printf("------------------------------\n");
-		printf("block : %d\n", i++);
-		printf("block size : %zu\n", sizeof(BLOCK));
-		printf("address : %p\n", (char*)blk + sizeof(blk));
-		printf("size : %zd\n", blk->size);
-		printf("state : %s\n", getStateString(blk->state));
-		printf("------------------------------\n\n");
-		break;
-		
-	
-	}
+    BLOCK* current = (BLOCK*)((char*)ptr - sizeof(BLOCK));
 
+    current->state = FREE;
 
-	//while (g_pFirstBlock != NULL)
-	//{
+    if (current->pPrev != NULL)
+    {
+        mergeBlock(current->pPrev);
+    }
+    mergeBlock(current);
+}
 
-	//	g_pFirstBlock = g_pFirstBlock->pNext;
-	//}
+void printHeap(void)
+{
+    BLOCK* pCurrent = g_pFirstBlock;
+
+    int index = 0;
+
+    printf("\n========== Heap ==========\n\n");
+
+    while (pCurrent != NULL)
+    {
+        printf("Block #%d\n", index++);
+        printf("Header Address : %p\n", (void*)pCurrent);
+        printf("User Address   : %p\n",
+            (void*)((char*)pCurrent + sizeof(BLOCK)));
+        printf("Size           : %zu\n", pCurrent->size);
+        printf("State          : %s\n",
+            getStateString(pCurrent->state));
+        printf("Prev           : %p\n",
+            (void*)pCurrent->pPrev);
+        printf("Next           : %p\n",
+            (void*)pCurrent->pNext);
+        printf("\n");
+
+        pCurrent = pCurrent->pNext;
+    }
+
+    printf("==========================\n");
 }
 
 int main(void)
 {
-	Init();
-	printHeap(g_pFirstBlock);
-	/*printf("%d %s %p %p",
-		g_pFirstBlock->size, getStateString(g_pFirstBlock->state),
-		g_pFirstBlock->pPrev, g_pFirstBlock->pNext);*/
+    initAllocator();
+    printf("초기 Heap\n");
+    printHeap();
+
+    void* p1 = myMalloc(100);
+
+    printf("100Byte 할당\n");
+    printHeap();
+
+    void* p2 = myMalloc(200);
+
+    printf("200Byte 할당\n");
+    printHeap();
+
+    void* p3 = myMalloc(150);
+
+    printf("150Byte 할당\n");
+    printHeap();
 
 
+    printf("첫 번째 Block 해제\n");
+    myFree(p1);
+    printHeap();
 
-	return 0;
+
+    printf("두 번째 Block 해제\n");
+    myFree(p2);
+    printHeap();
+
+
+    printf("세 번째 Block 해제\n");
+    myFree(p3);
+    printHeap();
+
+    return 0;
+
+
 }
